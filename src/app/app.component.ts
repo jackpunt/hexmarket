b63dd082805f679d0f6e6e02aa5655814fe638b1
jackpunt <jpeckj@gmail.com>
2024-11-06 09:48:47 -0800

    standalone: bootstrapApplication()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component, HostListener, Inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { KeyBinder } from '@thegraid/easeljs-lib';
 import type { Event } from '@thegraid/easeljs-module';
+import { StageComponent } from './stage/stage.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [StageComponent]
 })
 export class AppComponent {
   get title() { return this.titleService.getTitle(); }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
+
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { KeyBinder } from '@thegraid/easeljs-lib';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule),
+        KeyBinder
+    ]
+})
   .catch(err => console.error(err));
